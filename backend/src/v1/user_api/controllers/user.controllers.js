c3d30a765a41f2d2b0f576ae4d85adf1723b9194
tai <nguyentientai10@gmail.com>
2022-09-22 21:12:25 +0700

    edit router

--- backend/src/v1/user_api/controllers/user.controllers.js
+++ backend/src/v1/user_api/controllers/user.controllers.js
@@ -1,8 +1,8 @@
 const { returnReasons } = require("../../middlewares/handleError");
-// const {
-//   checkLoginUser,
-//   checkLoginGoogle,
-// } = require("../../user_api/services/user.service");
+const {
+  checkLoginUser,
+  checkLoginGoogle,
+} = require("../../user_api/services/user.service");
 
 const userCtrl = {
   loginUser: async (req, res) => {
@@ -10,19 +10,19 @@ const userCtrl = {
       const { email_phone, password, token } = req.body;
       const GetIPUser =
         req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-      // const { status, success, element } = await checkLoginUser({
-      //   email_phone,
-      //   password,
-      //   token,
-      //   GetIPUser,
-      //   res,
-      // });
-      // return res.status(status).json({
-      //   status,
-      //   success,
-      //   msg: returnReasons(status.toString()),
-      //   element,
-      // });
+      const { status, success, element } = await checkLoginUser({
+        email_phone,
+        password,
+        token,
+        GetIPUser,
+        res,
+      });
+      return res.status(status).json({
+        status,
+        success,
+        msg: returnReasons(status.toString()),
+        element,
+      });
     } catch (error) {
       return res.status(503).json({
         status: 503,
--- backend/src/v1/user_api/services/user.service.js
+++ backend/src/v1/user_api/services/user.service.js
@@ -1,17 +1,17 @@
-const CONSTANTS = require("../../configs/constants");
+// const CONSTANTS = require("../../configs/constants");
 const {
   createAccessToken,
   createRefreshToken,
   randomString,
 } = require("../../utils/helper");
-const {
-  LoginEmail,
-  LoginPhone,
-  LoginGoogle,
-  RegisterSocial,
-} = require("./userType.service");
+// const {
+//   LoginEmail,
+//   LoginPhone,
+//   LoginGoogle,
+//   RegisterSocial,
+// } = require("./userType.service");
 const { UserSpam } = require("./userSpam.service");
-const { callDataGoogle, saveCookies } = require("../../utils/storage");
+// const { callDataGoogle, saveCookies } = require("../../utils/storage");
 
 module.exports = {
   checkLoginUser: async ({ email_phone, password, token, GetIPUser, res }) => {
@@ -28,9 +28,9 @@ module.exports = {
     }
     let result_user = null;
     if (email_phone.includes("@") == true) {
-      result_user = await LoginEmail(email_phone, password);
+      // result_user = await LoginEmail(email_phone, password);
     } else {
-      result_user = await LoginPhone(email_phone);
+      // result_user = await LoginPhone(email_phone);
     }
     if (!result_user || result_user.success === false) {
       return {
@@ -51,47 +51,47 @@ module.exports = {
     };
   },
   checkLoginGoogle: async ({ tokenId, res }) => {
-    const user_google = await callDataGoogle(tokenId);
-    const { name, email, picture } = user_google.payload;
-    let result_user = await LoginGoogle(email);
-    if (result_user) {
-      const accessToken = createAccessToken({ id: result_user._id });
-      const refreshToken = createRefreshToken({ id: result_user._id });
-      saveCookies(res, refreshToken);
-      return {
-        status: 200,
-        success: true,
-        element: {
-          accessToken,
-          refreshToken,
-        },
-      };
-    } else {
-      var password = randomString(10);
-      let newUser = await RegisterSocial({
-        name,
-        email,
-        picture,
-        password,
-      });
-      if (!newUser) {
-        return {
-          status: 503,
-          success: false,
-        };
-      }
-      const accessToken = createAccessToken({ id: newUser._id });
-      const refreshToken = createRefreshToken({ id: newUser._id });
-      saveCookies(res, refreshToken);
-      return {
-        status: 200,
-        success: true,
-        element: {
-          accessToken,
-          refreshToken,
-        },
-      };
-    }
+    // const user_google = await callDataGoogle(tokenId);
+    // const { name, email, picture } = user_google.payload;
+    // let result_user = await LoginGoogle(email);
+    // if (result_user) {
+    //   const accessToken = createAccessToken({ id: result_user._id });
+    //   const refreshToken = createRefreshToken({ id: result_user._id });
+    //   saveCookies(res, refreshToken);
+    //   return {
+    //     status: 200,
+    //     success: true,
+    //     element: {
+    //       accessToken,
+    //       refreshToken,
+    //     },
+    //   };
+    // } else {
+    //   var password = randomString(10);
+    //   let newUser = await RegisterSocial({
+    //     name,
+    //     email,
+    //     picture,
+    //     password,
+    //   });
+    //   if (!newUser) {
+    //     return {
+    //       status: 503,
+    //       success: false,
+    //     };
+    //   }
+    //   const accessToken = createAccessToken({ id: newUser._id });
+    //   const refreshToken = createRefreshToken({ id: newUser._id });
+    //   saveCookies(res, refreshToken);
+    //   return {
+    //     status: 200,
+    //     success: true,
+    //     element: {
+    //       accessToken,
+    //       refreshToken,
+    //     },
+    //   };
+    // }
   },
   checkLoginFacebook: async ({}) => {},
 };
