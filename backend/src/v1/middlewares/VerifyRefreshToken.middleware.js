c3068febafc8a3e780b38a9f23db8c4a638122d2
tai <nguyentientai10@gmail.com>
2022-10-15 09:14:34 +0700

    #67 fixbug-backend-msg

--- backend/src/v1/middlewares/VerifyRefreshToken.middleware.js
+++ backend/src/v1/middlewares/VerifyRefreshToken.middleware.js
@@ -11,22 +11,30 @@ const VerifyRefreshToken = async (req, res, next) => {
     const refetch_user = await get(decoded.id);
     if (refetch_user === null) {
       return res.status(401).json({
+        success: false,
         status: false,
-        message: "Invalid request. Token is not in store.",
+        element: {
+          message: "Invalid request. Token is not in store.",
+        },
       });
     }
     if (refetch_user != token) {
       return res.status(401).json({
         status: 401,
-        message: "Invalid request. Token is not same in store.",
+        success: false,
+        element: {
+          message: "Invalid request. Token is not same in store.",
+        },
       });
     }
     next();
   } catch (error) {
     return res.status(401).json({
       status: 401,
-      message: "Your session is not valid.",
-      error,
+      success: false,
+      element: {
+        message: "Your session is not valid.",
+      },
     });
   }
 };
